4a50bf996abae3cea1164f0488e701575a7eb844
wind 94wind <wind94wind@163.com>
2024-03-22 08:20:44 +0800

    first commit

---
+++ src/main/java/com/pet_clinic_end/PetClinicEndApplication.java
@@ -0,0 +1,14 @@
+package com.pet_clinic_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicEndApplication.class, args);
+        test.hello();
+    }
+
+}
---
+++ src/main/java/com/pet_clinic_end/test.java
@@ -0,0 +1,8 @@
+package com.pet_clinic_end;
+
+public class test {
+    public static void hello()
+    {
+        System.out.println("Hello World");
+    }
+}
