4e247743167e345b26c725c4ed4b138495c7fbf8
tsyxxxka <tsyxxxka@gmail.com>
2024-03-29 10:43:29 +0800

    Fix impl issue for user utils

--- src/main/java/com/pet_clinic_end/mapper/UserMapper.java
+++ src/main/java/com/pet_clinic_end/mapper/UserMapper.java
@@ -1,13 +1,15 @@
 package com.pet_clinic_end.mapper;
 
+import com.pet_clinic_end.entity.Item;
 import com.pet_clinic_end.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User>{
 //    User getUserByEmail(@Param("email") String email);
     User getUserByEmail(@Param("email") String email);
     void addCommonUser(@Param("email") String email, @Param("name") String name, @Param("password") String password);
